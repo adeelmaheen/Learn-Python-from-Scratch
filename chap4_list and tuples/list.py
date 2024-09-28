326072cf98abfee3c13ed9f352b6329df3aafb2d
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2024-09-28 13:43:38 +0500

    add

---
+++ chap4_list and tuples/list.py
@@ -0,0 +1,16 @@
+# Python list are containers to store a set of values of any data type.
+# List are mutable we can change the exsisting list.
+
+friends:list=['Maheen',"Mahira","Misbah",23,1,False,23.22]
+# so we have string integer float and boolean in list we can store any type of data init.
+
+# List indexing:
+# A list can be indexed just like a string.
+
+list_1 = [7 , 9 , "Maheen"]
+print(list_1[0]) 
+print(list_1[1])
+print(list_1[70])
+print(list_1[0:2])
+
+
---
+++ chap4_list and tuples/list_methods.py
@@ -0,0 +1,51 @@
+# List Methods:
+list_1:list =[1,8,7,2,21,15,1,1]
+
+# append an element to a list:
+list_1.append(6)
+print(list_1)
+
+# insert an element at a specific position
+list_1.insert(2,"A")
+print(list_1)
+
+#extend a list by adding multiple elements
+list_1.extend([20,10,25])
+print(list_1)
+
+#remove an element from a list
+list_1.remove('A')
+print(list_1)
+
+#pop an element from a list
+a= list_1.pop()
+print(a)
+print(list_1)
+
+#get the index of an element (if not found through error)
+list_1.index(1)
+print(list_1)
+
+# count occurenece of an element
+count = list_1.count(1)
+print(count)
+
+# sort a list
+list_1.sort()
+print(list_1)
+
+# reverse a list
+list_1.reverse()
+print(list_1)
+
+# clear a list
+# list_1.clear()
+# print(list_1)
+
+# list comprehension ( to create lists based on existing lists)
+squares = [ x**2 for x in list_1]
+print(squares)
+
+#check if an element exists in a list
+if 1 in list_1:
+    print("1 is in the list")
