b0d85dc48ef582791dc48ffbddfbf7f17694e835
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2024-10-25 00:27:42 +0500

    add

--- chap7_loops/practice_set7/problem5.py
+++ chap7_loops/practice_set7/problem5.py
@@ -1,2 +1,10 @@
 # write a program to finD the sum of first natural numbers in while loop
 input_user = int(input("Enter the Number: "))
+
+i = 1
+sum = 0
+while(i <= input_user):
+    sum += i
+    i += 1
+    
+print(sum)
