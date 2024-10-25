a8362514a940344d17b3f9dd9b00c16b87876efe
adeelmaheen <157056396+adeelmaheen@users.noreply.github.com>
2024-10-26 01:27:00 +0500

    commit

---
+++ chap8_Functions/Recursion_function.py
@@ -0,0 +1,2 @@
+# Recursion is a function which calls itself.
+# It is used to directly , use a mathematical formulas function 
--- chap8_Functions/default_parameter_value.py
+++ chap8_Functions/default_parameter_value.py
@@ -1,1 +1,11 @@
-# we can have a value as default a
+# we can have a value as default as default argumentin a function.
+# if we specify name = " Maheen " in the containing def , this value is used when no argument is passed.
+
+def sum ( a,b,c = 10):
+    sum = a + b + c 
+
+    return sum
+
+defaulit_def = sum(12,22)
+print(defaulit_def)
+
